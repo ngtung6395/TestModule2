ebe3da12760b3bb44916a715f293e8ad15e6b7cf
ngtung-scoure <ngtung6395.com>
2021-05-12 11:44:43 +0700

    ktra module 2

--- src/views/Client.java
+++ src/views/Client.java
@@ -186,8 +186,8 @@ public class Client {
         if (contact == null) {
             System.out.println("Không có danh bạ có số điện thoại " + phoneNumber);
         } else {
+            System.out.println("Bạn có muốn xóa contact này không (Có/Không) : ")
             if (acceptDelete()){
-                System.out.println("Bạn có muốn xóa contact này không (Có/Không) : ");
                 MANAGER.removeTelephoneDirectory(contact);
             }
         }
